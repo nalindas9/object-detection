7d733ae5a690299fcd531e0a997fb5440f03cfb6
Nalin Das <nalindas9@gmail.com>
2022-02-16 22:36:42 -0800

    Modify configs and config parser

--- configuration_parser.py
+++ configuration_parser.py
@@ -8,16 +8,17 @@ class ConfigurationParser:
     def __init__(self, path=None, module_name=None):
         """
         """
-        self.yaml_path = path
-        self.yaml_config_dict = self.parseYamlFile()
-        self.json_path = self.getJsonConfigPath(module_name)
-        self.config_dict = self.parseJsonFile()
-
+        self._yaml_path = path
+        self._module_name = module_name
+        self._yaml_config_dict = self.parseYamlFile()
+        self._json_path = self.getJsonConfigPath()
+        self._config_dict = self.parseJsonFile()
+        
     def parseJsonFile(self):
         """
         """
         config_dict = {}
-        file = open(self.json_path, 'r')
+        file = open(self._json_path, 'r')
         config_dict = json.load(file)
         return config_dict
 
@@ -25,16 +26,31 @@ class ConfigurationParser:
         """
         """
         config_dict = {}
-        file = open(self.yaml_path, 'r')
+        file = open(self._yaml_path, 'r')
         config_dict = yaml.load(file, Loader=yaml.FullLoader)
         return config_dict
 
-    def getJsonConfigPath(self, module_name):
+    def getJsonConfigPath(self):
         """
         """
-        return self.parseYamlFile()[module_name]['config_path']
+        return self._yaml_config_dict[self._module_name]['config_path']
+
+    def getModelConfigPath(self):
+        """
+        """
+        return self._yaml_config_dict[self._module_name]['model']['config_path']
+
+    def getModelPath(self):
+        """
+        """
+        return self._yaml_config_dict[self._module_name]['model']['model_path']
+
+    def getMinConfidence(self):
+        """
+        """
+        return self._yaml_config_dict[self._module_name]['model']['min_confidence']
 
     def getConfigValue(self, key):
         """
         """
-        return self.config_dict[key]
+        return self._config_dict[key]
