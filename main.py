480fd35bf70cf5cbbf577bbcf2f9525a838faff0
Nalin Das <nalindas9@gmail.com>
2022-02-16 22:41:07 -0800

    Modify main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from detect import Detect
 
+def main():
+    """
+    Main function
+    """
+    detect = Detect() # Create Detect object
+    network = detect.initializeModel() # Initialize model
+    detect.detect(network=network) # Detect
+    
+
 if __name__ == '__main__':
-    detect = Detect() # instantiate Detect class
-    detect.detect()
+    main()
